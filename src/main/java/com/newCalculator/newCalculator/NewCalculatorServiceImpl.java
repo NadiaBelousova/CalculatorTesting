5d794cbfae06c461b05fcc369e0995ac2ebd20cd
Надежда <nadin94403@gmail.com>
2023-11-26 21:36:31 +0300

    1

---
+++ src/main/java/com/newCalculator/newCalculator/NewCalculatorServiceImpl.java
@@ -0,0 +1,5 @@
+package com.newCalculator.newCalculator;
+
+public class NewCalculatorServiceImpl {
+
+}
---
+++ src/test/java/com/newCalculator/newCalculator/NewCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newCalculator.newCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
